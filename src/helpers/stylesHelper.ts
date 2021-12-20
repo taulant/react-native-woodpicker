f2527df9045fc5dad2d507008f0ac7d042c94c79
Thomas Dubois <thomas.dubois@rednet.io>
2021-12-20 09:28:24 +0100

    feat(android): add onClose and onOpen callback support for android picker

--- src/helpers/stylesHelper.ts
+++ src/helpers/stylesHelper.ts
@@ -80,9 +80,9 @@ export const styles = StyleSheet.create<Style>({
     opacity: 0,
     position: "absolute",
     top: 0,
-    bottom: 0,
-    right: 0,
     left: 0,
+    height: 0,
+    width: 0,
   },
   hiddenDateTimePicker: {
     position: "absolute",
